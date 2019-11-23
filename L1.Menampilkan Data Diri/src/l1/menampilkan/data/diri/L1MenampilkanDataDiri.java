7ca8eb5b5f95e435c2b32175fec03028ca7bff49
Indrawan Sr <55901721+Indrawan201130@users.noreply.github.com>
2019-11-23 12:52:44 +0700

    Add files via upload

---
+++ L1.Menampilkan Data Diri/src/l1/menampilkan/data/diri/L1MenampilkanDataDiri.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1.menampilkan.data.diri;
+
+/**
+ * NAMA  : Indrawan Surya R
+ * NIM   : A2.1900084
+ * Kelas : TI-1C
+ * 
+ * @author IdrawanSuryaR
+ */
+public class L1MenampilkanDataDiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata" + "\n" + "-----------------" );
+        System.out.println("NAMA\t\t         : Indrawan Surya R");
+        System.out.println("ALAMAT\t\t         : Cisitu Kab.Sumedang ");
+        System.out.println("TEMPAT LAHIR\t\t : Sumedang ");
+        System.out.println("TANGGAL LAHIR\t\t : 30 November 2000");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: Bermain Game");
+        System.out.println("MAKES\t\t: Martabak");
+        System.out.println("MIKES\t\t: Coffee");
+    }
+    
+}
